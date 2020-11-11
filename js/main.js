dcc0c51fe037094cf5e6a0c320e1525eb616e3fc
unknown <bogdanvoskanov@gmail.com>
2020-11-11 18:34:58 +0300

    Adaprive and navigation menu button

--- js/main.js
+++ js/main.js
@@ -1,6 +1,27 @@
 "use strict";
 
-$("[data-fancybox]").fancybox({
-   youtube: {
-   }
+const navBtn = document.querySelector('.nav__btn');
+const navMenu = document.querySelector('.nav-menu');
+
+navBtn.addEventListener('click', () => {
+   navMenu.classList.toggle('nav-menu--active');
+   navBtn.classList.toggle('active')
+})
+
+$(".heroes__slider").slick({
+   slidesToShow: 1,
+   slidesToScroll: 1,
+   arrows: true,
+   prevArrow: '<button type="button" class="slick-prev"></button>',
+   nextArrow: '<button type="button" class="slick-next"></button>',
+   responsive: [
+      {
+         breakpoint: 768,
+         settings: {
+            arrows: false,
+         },
+      },
+   ],
 });
+
+
