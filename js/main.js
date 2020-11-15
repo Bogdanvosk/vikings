45d11bfeae0e34aa92ebc9c09acd3c59d230ef29
unknown <bogdanvoskanov@gmail.com>
2020-11-16 02:19:55 +0300

    Fix in script

--- js/main.js
+++ js/main.js
@@ -1,29 +1,26 @@
 "use strict";
+$(document).ready(function() {
+    new WOW().init();
 
-new WOW().init();
+    const navBtn = document.querySelector('.nav__btn');
+    const navMenu = document.querySelector('.nav-menu');
 
-const navBtn = document.querySelector('.nav__btn');
-const navMenu = document.querySelector('.nav-menu');
+    navBtn.addEventListener('click', () => {
+        navMenu.classList.toggle('nav-menu--active');
+        navBtn.classList.toggle('active')
+    })
 
-navBtn.addEventListener('click', () => {
-   navMenu.classList.toggle('nav-menu--active');
-   navBtn.classList.toggle('active')
+    $(".heroes__slider").slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true,
+        prevArrow: '<button type="button" class="slick-prev"></button>',
+        nextArrow: '<button type="button" class="slick-next"></button>',
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+                arrows: false,
+            },
+        }, ],
+    });
 })
-
-$(".heroes__slider").slick({
-   slidesToShow: 1,
-   slidesToScroll: 1,
-   arrows: true,
-   prevArrow: '<button type="button" class="slick-prev"></button>',
-   nextArrow: '<button type="button" class="slick-next"></button>',
-   responsive: [
-      {
-         breakpoint: 768,
-         settings: {
-            arrows: false,
-         },
-      },
-   ],
-});
-
-
